53bfa50e5fb0ac681e229d8660cc5a90fca9ceba
ikokostya <ikokostya@gmail.com>
2016-09-29 11:02:36 +0300

    Update eslint rules (fixes #67) (#73)

--- packages/eslint-config-loris/es6.js
+++ packages/eslint-config-loris/es6.js
@@ -8,6 +8,7 @@ module.exports = {
     rules: {
         // http://eslint.org/docs/rules/#best-practices
         'class-methods-use-this': 2,
+        'no-empty-pattern': 2,
         'no-loop-func': 0, // This is not a problem with `let` declarations.
 
         // http://eslint.org/docs/rules/#ecmascript-6
@@ -17,8 +18,11 @@ module.exports = {
         'no-class-assign': 2,
         'no-const-assign': 2,
         'no-dupe-class-members': 2,
+        'no-new-symbol': 2,
         'no-this-before-super': 2,
         'no-useless-computed-key': 2,
+        'no-useless-constructor': 2,
+        'no-useless-rename': 2,
         'no-var': 2,
         'prefer-arrow-callback': 2,
         'prefer-const': 2,
